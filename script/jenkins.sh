4ad09279e8603ef84759216f08736de81f33a28f
Martin Lewis <mlewis000@yahoo.co.uk>
2021-04-19 09:07:00 +0100

    Script sleep update

--- script/jenkins.sh
+++ script/jenkins.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
+sleep 120
 sudo rpm -ivh http://yum.puppet.com/puppet6/puppet6-release-el-7.noarch.rpm && sudo yum install puppet-agent -y
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo /opt/puppetlabs/puppet/bin/puppet module install puppet-jenkins --version 2.0.0
 sudo cp /tmp/site.pp /etc/puppetlabs/code/environments/production/manifests/
+sleep 60
 sudo /opt/puppetlabs/puppet/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests/site.pp
-#sudo cp jenkins /etc/sysconfig/jenkins
-#sudo systemctl restart jenkins
 
