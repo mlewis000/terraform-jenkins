ad9da9e11fdf0410e5cc5c0c154f9395459955a8
Martin Lewis <mlewis000@yahoo.co.uk>
2021-04-19 09:40:20 +0100

    Script update

--- script/jenkins.sh
+++ script/jenkins.sh
@@ -2,6 +2,7 @@
 
 # Now  install puppet agent - give the EC2 a chance to install properly.
 sleep 120
+
 sudo rpm -ivh http://yum.puppet.com/puppet6/puppet6-release-el-7.noarch.rpm && sudo yum install puppet-agent -y
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo /opt/puppetlabs/puppet/bin/puppet module install puppet-jenkins --version 2.0.0
@@ -12,8 +13,19 @@ sleep 120
 # Now apply the module
 sudo /opt/puppetlabs/puppet/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests/site.pp
 
-echo "Now please copy the aws_instance_public_dns output (Below).
+
+echo "==========================================="
+echo "==========================================="
+echo " "
+echo " "
+echo " "
+echo "Now please copy the aws_instance_public_dns output (Below)."
 echo "Test jenkinks via http, as in."
 echo "http://${external-dns-name-provide}:8000"
+echo " "
+echo " "
+echo " "
+echo "==========================================="
+echo "==========================================="
 
 
