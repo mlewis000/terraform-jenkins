44c6588811d9f0edf0f28f84860148e79568132e
Martin Lewis <mlewis000@yahoo.co.uk>
2021-04-19 09:29:21 +0100

    updated scripts

--- files/site.pp
+++ files/site.pp
@@ -1,7 +1,7 @@
 # Copied to /etc/puppetlabs/code/environments/production/manifests/site.pp
 # site.pp is the main manifest for classifying with classes from component modules
 # node default means that when the puppet apply command is executed every node will
-#have this class incuded
+# have this class incuded.
 
 node 'default' {
 
@@ -9,14 +9,15 @@ node 'default' {
 
   class { 'jenkins':
 
-# https://github.com/voxpupuli/puppet-jenkins/issues/659 explains the hash configurations - paramets here are passed as a hash value
+# https://github.com/voxpupuli/puppet-jenkins/issues/659 explains the hash configurations - paramets here are passed as a hash value.
+# Initially specified the one port, used this solution instead of copying in a jenkins config file with the 8000 port included.
 
     config_hash => {
        'HTTP_PORT' => { 'value' => '8000'},
        'JENKINS_PORT' => { 'value' => '8000'}
     },
 
-# cli disabled because module fails probably due to backward comapitbility but not needed for this exercise
+# cli disabled because module fails probably due to backward comapitbility but not needed for this exercise.
     cli => false,
   }
 }
--- script/jenkins.sh
+++ script/jenkins.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+# Now  install puppet agent - give the EC2 a chance to install properly.
 sleep 120
 sudo rpm -ivh http://yum.puppet.com/puppet6/puppet6-release-el-7.noarch.rpm && sudo yum install puppet-agent -y
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo /opt/puppetlabs/puppet/bin/puppet module install puppet-jenkins --version 2.0.0
 sudo cp /tmp/site.pp /etc/puppetlabs/code/environments/production/manifests/
-sleep 60
+
+sleep 120
+
+# Now apply the module
 sudo /opt/puppetlabs/puppet/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests/site.pp
+
+echo "Now please copy the aws_instance_public_dns output (Below).
+echo "Test jenkinks via http, as in."
+echo "http://${external-dns-name-provide}:8000"
+
 
